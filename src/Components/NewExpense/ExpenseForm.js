9e2ce8b680c091576cb58bb07c0a901bc12515f5
himalayanegi10 <himalayanegi10@gmail.com>
2023-10-24 20:40:18 +0530

    Handling form submission

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -51,8 +51,20 @@ const ExpenseForm = () => {
         }
     };
 
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+    };
+
     return (
-        <form>
+        <form onSubmit={(event) => {
+            formSubmitHandler(event);
+        }}>
             <div className="new-expense__controls">
                 <div className="new-expense__controls">
                     <label> Title </label>
