ed20d5d8de0ffd6dcd1a8f1c43058e25f7a43993
himalayanegi10 <himalayanegi10@gmail.com>
2023-10-29 18:45:57 +0530

    Added ExpensesFilter

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -1,26 +1,35 @@
 import ExpenseComponent from './ExpenseComponent';
 import './Expense.css';
 import Card from '../UI/Card';
-import React  from 'react';
+import React, {useState}  from 'react';
+import ExpenseFilter from '../ExpensesFilter/ExpensesFilter';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 
 function Expense (props) {
     const expenseArray = props.expenses;
-    return React.createElement(Card, {className: 'expenses'},
-            React.createElement(ExpenseComponent, {expense: expenseArray[0]}),
-            React.createElement(ExpenseComponent, {expense: expenseArray[1]}),
-            React.createElement(ExpenseComponent, {expense: expenseArray[2]}),
-            React.createElement(ExpenseComponent, {expense: expenseArray[3]}),
-        );
+    // return React.createElement(Card, {className: 'expenses'},
+    //         React.createElement(ExpenseComponent, {expense: expenseArray[0]}),
+    //         React.createElement(ExpenseComponent, {expense: expenseArray[1]}),
+    //         React.createElement(ExpenseComponent, {expense: expenseArray[2]}),
+    //         React.createElement(ExpenseComponent, {expense: expenseArray[3]}),
+    //     );
     // Below is a Syntactic Sugar for above code in React
     // We'll use below code throughout this course
-    // return (
-    //     <Card className='expenses'>
-    //         <ExpenseComponent expense={expenseArray[0]}/>
-    //         <ExpenseComponent expense={expenseArray[1]}/>
-    //         <ExpenseComponent expense={expenseArray[2]}/>
-    //         <ExpenseComponent expense={expenseArray[3]}/>
-    //     </Card>
-    // );
+    const [filterYear, setFilterYear] = useState('2019');
+    const onSelectMethod = (year) => {
+        setFilterYear(parseInt(year));
+        console.log("Inside Expense Component and filter year = " + year);
+    };
+
+    return (
+        <Card className='expenses'>
+            <ExpensesFilter onSelectYear={onSelectMethod} selectedYear={filterYear}/>
+            <ExpenseComponent expense={expenseArray[0]}/>
+            <ExpenseComponent expense={expenseArray[1]}/>
+            <ExpenseComponent expense={expenseArray[2]}/>
+            <ExpenseComponent expense={expenseArray[3]}/>
+        </Card>
+    );
 }
 
 export default Expense;
