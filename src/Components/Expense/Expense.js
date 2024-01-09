91d39f124a6a22e99b88fb8a3b41cd418094f67a
himalayanegi10 <himalayanegi10@gmail.com>
2024-01-10 00:32:37 +0530

    applying filter to expense list

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -6,7 +6,6 @@ import ExpenseFilter from '../ExpensesFilter/ExpensesFilter';
 import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 
 function Expense (props) {
-    const expenseArray = props.expenses;
     // return React.createElement(Card, {className: 'expenses'},
     //         React.createElement(ExpenseComponent, {expense: expenseArray[0]}),
     //         React.createElement(ExpenseComponent, {expense: expenseArray[1]}),
@@ -15,10 +14,14 @@ function Expense (props) {
     //     );
     // Below is a Syntactic Sugar for above code in React
     // We'll use below code throughout this course
-    const [filterYear, setFilterYear] = useState('2019');
+    const [filterYear, setFilterYear] = useState(2019);
+
+    const expenseArray = props.expenses.filter(expense => {
+        return expense.date.getFullYear() === filterYear;
+    });
+    
     const onSelectMethod = (year) => {
         setFilterYear(parseInt(year));
-        console.log("Inside Expense Component and filter year = " + year);
     };
 
     return (
